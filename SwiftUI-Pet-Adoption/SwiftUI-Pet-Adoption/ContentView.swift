2ea389032cf4838ee8c289b4a10c8a0c9404a17e
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2022-03-20 07:58:17 +0700

    init SwiftUI project

---
+++ SwiftUI-Pet-Adoption/SwiftUI-Pet-Adoption/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Pet-Adoption
+//
+//  Created by Le Hoang Anh on 20/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
